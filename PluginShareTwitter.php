ff0f1e1ceb38b7303d1753711f6c843cb6259dc2
Claes Östlund <claes@stenbergit.se>
2024-03-07 16:50:16 +0100

    1.0.0

---
+++ PluginShareTwitter.php
@@ -0,0 +1,26 @@
+<?php
+class PluginShareTwitter{
+  function __construct() {
+    wfPlugin::includeonce('wf/yml');
+  }
+  public function page_demo(){
+    wfPlugin::enable('share/twitter');
+    wfPlugin::enable('icons/bootstrap_v1_8_1');
+    $widget = wfDocument::createWidget('share/twitter', 'button_share_page');
+    wfDocument::renderElement(array($widget));
+  }
+  public function widget_button_share_page($data){
+    $data = new PluginWfArray($data);
+    if(!$data->get('data/u')){
+      if(wfRequest::get('u')){
+        $data->set('data/u', wfRequest::get('u'));
+      }else{
+        $data->set('data/u', wfServer::calcUrl(true));
+      }
+    }
+    $data->set('data/href', 'http://twitter.com/share?url='.$data->get('data/u').'&hashtags='.$data->get('data/hashtags').'&text='.$data->get('data/text'));
+    $element = wfDocument::getElementFromFolder(__DIR__, __FUNCTION__);
+    $element->setByTag($data->get('data'));
+    wfDocument::renderElement($element);
+  }
+}
---
+++ README.md
@@ -0,0 +1,64 @@
+# Buto-Plugin-ShareTwitter
+
+
+
+<a name="key_0"></a>
+
+## Settings
+
+
+
+<a name="key_1"></a>
+
+## Usage
+
+
+
+<a name="key_2"></a>
+
+## Pages
+
+
+
+<a name="key_2_0"></a>
+
+### page_demo
+
+
+
+<a name="key_3"></a>
+
+## Widgets
+
+
+
+<a name="key_3_0"></a>
+
+### widget_button_share_page
+
+
+
+<a name="key_4"></a>
+
+## Event
+
+
+
+<a name="key_5"></a>
+
+## Construct
+
+
+
+<a name="key_5_0"></a>
+
+### __construct
+
+
+
+<a name="key_6"></a>
+
+## Methods
+
+
+
